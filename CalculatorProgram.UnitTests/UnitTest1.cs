eee8e60d1d7d3eb150d8b32fe5a236672f55d7b4
Sulmaz <Sulmaz@DESKTOP-QTGHBCO>
2020-11-09 10:34:49 +0330

    Add Unit Test

---
+++ CalculatorProgram.UnitTests/UnitTest1.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Reflection.Metadata;
+using Xunit;
+
+namespace CalculatorProgram.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test_AddTwoElements()
+        {
+            //Arrange 
+            Calculator calculator=new Calculator();
+
+            //Act
+            var result = calculator.Sum(1,1);
+
+            //Assert
+            Assert.Equal(2,result);
+        }
+
+        [Fact]
+        public void Test_AddManyElements()
+        {
+            //Arrange
+            Calculator calculator=new Calculator();
+            
+            //Action
+            var result = calculator.Sum(0.5M, 3, 2, 1, -3, 6.5M);
+
+            //Assert
+            Assert.Equal(10, result);
+            //Assert.IsNotType<Decimal>(result);
+        }
+    }
+}
---
+++ CalculatorProgram/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorProgram
+{
+  public  class Calculator
+    {
+
+        public decimal Sum(params decimal[] numbers)
+        {
+            var result = 0.0M;
+
+            foreach (var number in numbers)
+            {
+                result += number;
+            }
+
+            return result;
+        }
+    }
+}
