e5b4505b49a4596e6d694cbff4fe8ee2b6ca567d
Sulmaz <Sulmaz@DESKTOP-QTGHBCO>
2020-11-11 18:56:24 +0330

    add integration test

---
+++ WebCalculator.IntegrationTests/WebCalculatorTests.cs
@@ -0,0 +1,29 @@
+﻿using Calculator.IntegrationTests;
+using FluentAssertions;
+using Xunit;
+
+namespace WebCalculator.IntegrationTests
+{
+   public class WebCalculatorTests
+    {
+        [Fact]
+        public async void Test_WebCalculatorSum()
+        {
+            //Arrange
+            var context=new TestContext();
+
+            //Act
+            using (var client=context.Client)
+            {
+                var response = await client.GetAsync("/api/calculator/sum?x=4&y=9");
+                //Assert.True(response.IsSuccessStatusCode);
+                response.IsSuccessStatusCode.Should().Be(true);
+
+                var content =await response.Content.ReadAsStringAsync();
+                //Assert.Equal("36.0",content);
+                content.Should().Be("13.0");
+            }
+
+        }
+    }
+}
---
+++ WebCalculator/Controllers/CalculatorController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CalculatorProgram;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebCalculator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet(nameof(Sum))]
+        public decimal Sum(decimal x,decimal y)
+        {
+            return new Calculator().Sum(x,y);
+        }
+    }
+}
